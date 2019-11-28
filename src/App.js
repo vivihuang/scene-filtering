8dcdca00d68dddbb32391df8d9f4d727c9ff1fd4
Vivi🐳Ruiwei Huang <rwhuang@thoughtworks.com>
2019-11-28 15:52:11 +0800

    Help to play multiple videos.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Hls from 'hls.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SOURCES from "./videoList";
+import './App.css';
+import Slider from "antd/lib/slider";
+import Icon from "antd/lib/icon";
+
+class App extends React.Component {
+    videoRefs = [React.createRef(), React.createRef(), React.createRef()];
+    hlsArr = ["", "", ""];
+
+    state = {
+        isPlaying: false,
+    }
+
+    componentDidMount() {
+        this.videoRefs.forEach((item, index) => {
+            const hls = new Hls();
+            this.hlsArr[index] = hls;
+            hls.loadSource(SOURCES[index].src);
+            hls.attachMedia(item.current);
+        })
+
+    }
+
+    componentWillUnmount() {
+        this.hlsArr.forEach((item) => {
+            if (item) {
+                item.destroy();
+            }
+        })
+    }
+
+    handlePlay = () => {
+        this.videoRefs.forEach((item) => {
+            item.current.play()
+        })
+        this.setState({
+            isPlaying: true
+        })
+    };
+
+    handlePause = () => {
+        this.videoRefs.forEach((item) => {
+            item.current.pause()
+        })
+        this.setState({
+            isPlaying: false
+        })
+    };
+
+    handleChange = (value) => {
+        this.videoRefs.forEach((item) => {
+            item.current.currentTime = value
+        })
+    };
+
+    render() {
+        const {isPlaying} = this.state;
+        return (
+            <div className="App">
+                <header>
+                    <h1>Test Video</h1>
+                </header>
+                <div className="videoList">
+                    {SOURCES.map((item, index) => (
+                        <video
+                            key={index}
+                            ref={this.videoRefs[index]}
+                            width="400"
+                            poster={item.poster}
+                            crossOrigin
+                            playsInline
+                        />
+                    ))}
+                </div>
+                <div className="icon-wrapper">
+                    {
+                        isPlaying
+                            ? <Icon type="pause-circle" onClick={this.handlePause}/>
+                            : <Icon type="play-circle" onClick={this.handlePlay}/>
+                    }
+                    <Slider onChange={this.handleChange}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
