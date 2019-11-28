cc17054688f0404a396320aead8d2fa5f1c2df94
Vivi🐳Ruiwei Huang <rwhuang@thoughtworks.com>
2019-11-28 17:10:39 +0800

    Refactor code to support correction.

--- src/App.js
+++ src/App.js
@@ -1,79 +1,57 @@
 import React from 'react';
-import Hls from 'hls.js';
 
-import SOURCES from "./videoList";
+import Left from "./Left";
 import './App.css';
-import Slider from "antd/lib/slider";
 import Icon from "antd/lib/icon";
+import Slider from "antd/lib/slider";
 
-class App extends React.Component {
-    videoRefs = [React.createRef(), React.createRef(), React.createRef()];
-    hlsArr = ["", "", ""];
+export default class App extends React.Component {
+    timer = null;
 
     state = {
+        currentTime: 0,
+        sliderTime: 0,
         isPlaying: false,
     }
 
-    componentDidMount() {
-        this.videoRefs.forEach((item, index) => {
-            const hls = new Hls();
-            this.hlsArr[index] = hls;
-            hls.loadSource(SOURCES[index].src);
-            hls.attachMedia(item.current);
-        })
-
-    }
-
-    componentWillUnmount() {
-        this.hlsArr.forEach((item) => {
-            if (item) {
-                item.destroy();
-            }
-        })
-    }
-
     handlePlay = () => {
-        this.videoRefs.forEach((item) => {
-            item.current.play()
-        })
         this.setState({
             isPlaying: true
         })
-    };
+        this.handlePlaying()
+    }
 
     handlePause = () => {
-        this.videoRefs.forEach((item) => {
-            item.current.pause()
-        })
         this.setState({
             isPlaying: false
         })
-    };
+        clearInterval(this.timer)
+    }
+
+    handlePlaying = () => {
+        this.timer = setInterval(() => {
+            this.setState((prevState) => ({
+                sliderTime: prevState.sliderTime + 1
+            }))
+        }, 1000)
+    }
 
     handleChange = (value) => {
-        this.videoRefs.forEach((item) => {
-            item.current.currentTime = value
+        this.setState({
+            currentTime: value,
+            sliderTime: value,
         })
-    };
+    }
 
     render() {
-        const {isPlaying} = this.state;
+        const {isPlaying, currentTime, sliderTime} = this.state;
         return (
-            <div className="App">
+            <div className="app">
                 <header>
                     <h1>Test Video</h1>
                 </header>
-                <div className="videoList">
-                    {SOURCES.map((item, index) => (
-                        <video
-                            key={index}
-                            ref={this.videoRefs[index]}
-                            width="400"
-                            poster={item.poster}
-                            crossOrigin
-                            playsInline
-                        />
-                    ))}
+                <div className="content">
+                    <Left isPlaying={isPlaying} currentTime={currentTime} sliderTime={sliderTime}/>
                 </div>
                 <div className="icon-wrapper">
                     {
@@ -81,11 +59,9 @@ class App extends React.Component {
                             ? <Icon type="pause-circle" onClick={this.handlePause}/>
                             : <Icon type="play-circle" onClick={this.handlePlay}/>
                     }
-                    <Slider onChange={this.handleChange}/>
+                    <Slider onChange={this.handleChange} max={888} value={sliderTime} />
                 </div>
             </div>
         );
     }
 }
-
-export default App;
---
+++ src/Left.js
@@ -0,0 +1,100 @@
+import React from "react";
+import Hls from "hls.js";
+
+const VIDEO_INFO = {
+    "poster": "https://bitdash-a.akamaihd.net/content/sintel/poster.png",
+    "src": "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
+}
+
+const VIDEO_AMOUNT = 4;
+
+export default class Left extends React.Component {
+    videoRefs = new Array(VIDEO_AMOUNT).fill("").map(() => React.createRef());
+    hlsArr = new Array(VIDEO_AMOUNT);
+
+    componentDidMount() {
+        this.videoRefs.forEach((item, index) => {
+            const hls = new Hls();
+            this.hlsArr[index] = hls;
+            hls.loadSource(VIDEO_INFO.src);
+            hls.attachMedia(item.current);
+        })
+
+    }
+
+    componentDidUpdate(prevProps) {
+        const { isPlaying, currentTime, sliderTime } = this.props;
+        if (isPlaying !== prevProps.isPlaying) { 
+            if (isPlaying) {
+                this.handlePlay()
+            } else {
+                this.handlePause()
+            }
+        }
+        if (currentTime !== prevProps.currentTime) {
+            this.handleChange(currentTime)
+        } else {
+            this.handleCorrection(sliderTime)
+        }
+    }
+
+    componentWillUnmount() {
+        this.hlsArr.forEach((item) => {
+            if (item) {
+                item.destroy();
+            }
+        })
+    }
+
+    handlePlay = () => {
+        this.videoRefs.forEach((item) => {
+            if (item.current && item.current.paused) {
+                item.current.play()
+            }
+        })
+
+    };
+
+    handlePause = () => {
+        this.videoRefs.forEach((item) => {
+            if (item.current && item.current.played) {
+                item.current.pause()
+            }
+        })
+    };
+
+    handleChange = (value) => {
+        this.videoRefs.forEach((item) => {
+            if (item.current.currentTime !== value) {
+                console.log(value, item.current.currentTime)
+                item.current.currentTime = value
+            }
+        })
+    };
+
+    handleCorrection = (value) => {
+        this.videoRefs.forEach((item) => {
+            if (item.current.currentTime <= value - 1) {
+                item.current.currentTime = value
+            }
+        })
+    };
+
+    render() {
+        return (
+            <div className="left">
+                <div className="videoList">
+                    {this.videoRefs.map((item, index) => (
+                        <video
+                            key={index}
+                            ref={item}
+                            width="600"
+                            poster={VIDEO_INFO.poster}
+                            crossOrigin
+                        />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
